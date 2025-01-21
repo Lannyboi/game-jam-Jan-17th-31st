1833ed3e694129f1d23112d8b5ea867c1890ff82
Landon Gray <106999821+Lannyboi@users.noreply.github.com>
2025-01-21 14:50:12 -0800

    Sprite update for player

--- SD Timer/SDTimer/player.gd
+++ SD Timer/SDTimer/player.gd
@@ -4,11 +4,6 @@ extends CharacterBody2D
 @export var inEnemy = false
 @export var enemysSelected = 0
 
-func _process(float):
-	if (enemysSelected > 0):
-		$Sprite2D.frame = 1
-	elif (enemysSelected == 0):
-		$Sprite2D.frame = 0
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
