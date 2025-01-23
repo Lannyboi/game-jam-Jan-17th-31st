eea2737f8e99c650ba4141684df2be0868c196f3
Fluffybob123 <ejs2009.es@gmail.com>
2025-01-23 15:12:05 -0800

    Tested Spread shot

--- SD Timer/All Combined/shot.gd
+++ SD Timer/All Combined/shot.gd
@@ -1,7 +1,6 @@
 extends Marker2D
 
 
-
 func _process(delta: float) -> void:
 	if $"..".inRobot == true:
 		look_at(get_global_mouse_position())
--- SD Timer/Rocket/Rocketenemy.gd
+++ SD Timer/Rocket/Rocketenemy.gd
@@ -19,6 +19,7 @@ func _ready() -> void:
 	$HeathBar.visible = true
 
 
+
 func _on_rocket_sprite_timeout() -> void:
 	$Sprite2D.frame = 0
 	$Bullet.start(cooldown / 2)
@@ -30,6 +31,18 @@ func _on_bullet_timeout() -> void:
 		owner.add_child(b)
 		b.transform = $Shot.global_transform
 		$RocketSprite.start(cooldown / 2)
+		
+		#$Sprite2D.frame = 1
+		#var b2 = bullet.instantiate()
+		#owner.add_child(b2)
+		#b2.transform = $Shot2.global_transform
+		#$RocketSprite.start(cooldown / 2)
+		
+		#$Sprite2D.frame = 1
+		#var b3 = bullet.instantiate()
+		#owner.add_child(b3)
+		#b3.transform = $Shot3.global_transform
+		#$RocketSprite.start(cooldown / 2)
 
 
 func _on_area_2d_area_entered(_area: Area2D) -> void:
