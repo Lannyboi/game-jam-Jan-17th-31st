9c4f06ac87a2cd495252dbf6d20767b3481091a0
Fluffybob123 <ejs2009.es@gmail.com>
2025-01-29 14:35:57 -0800

    How to play added

--- SD Timer/Menu/menu.gd
+++ SD Timer/Menu/menu.gd
@@ -15,3 +15,7 @@ func _on_settings_pressed():
 
 func _on_quit_pressed():
 		get_tree().quit()
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Menu/How-To-Play.tscn")
