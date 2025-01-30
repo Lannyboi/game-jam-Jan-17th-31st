f6feb52dc139891492d77176d81523938cbd0f01
Fluffybob123 <ejs2009.es@gmail.com>
2025-01-30 13:34:32 -0800

    Small Changes

--- SD Timer/All Combined/Globalvars.gd
+++ SD Timer/All Combined/Globalvars.gd
@@ -7,13 +7,13 @@ extends Node
 
 @export_category("Robots")
 @export var enemys = 0
-@export var enemycap = 3
+@export var enemycap = 4
 @export var RobotsHacked = 0
 
 @export_category("Virus")
-@export var virusesAtStart = 20
+@export var virusesAtStart = 100
 @export var virus = 0
-@export var viruscap = 2
+@export var viruscap = 3
 @export var virusleft = 0
 
 @export_category("Settings")
