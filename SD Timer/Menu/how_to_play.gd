73819a69231dd999c927b05a6813b1592dfb572c
Landon Gray <106999821+Lannyboi@users.noreply.github.com>
2025-01-29 20:55:30 -0800

    UI cleanup and custom buttons
    We can change the color to match the background once amber finishes

--- SD Timer/Menu/how_to_play.gd
+++ SD Timer/Menu/how_to_play.gd
@@ -1,8 +1,14 @@
 extends Control
 
-
-
-
-
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Menu/menu.tscn")
+
+
+func _on_next_pressed():
+	$HowToPlay1.visible = false
+	$HowToPlay2.visible = true
+
+
+func _on_previous_pressed():
+	$HowToPlay1.visible = true
+	$HowToPlay2.visible = false
